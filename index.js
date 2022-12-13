af72659d90b3d53272069bc706c6306d61bc871d
Brian <wamwangimathenge@gmail.com>
2022-12-13 12:40:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // Lowercase the string and use the RegExp to remove unwanted characters
+  const str = /[\W_]/g;
+  const lowerStr = word.toLowerCase().replace(str, '');
+  // Reversing the string
+  const revStr = lowerStr.split('').reverse().join('');
+  return revStr === lowerStr;
 }
+
+// isPalindrome("abba")
+// isPalindrome("racecar")
+// isPalindrome("a")
+// isPalindrome("robot")
+// isPalindrome("ab")
 
 /* 
   Add your pseudocode here
+  Declare a function called isPalindrome which receives one argument - word
+    Use built-in functions that will reverse the string.
+    Check if the reversed string is strictly equal to the normal string and return a boolean.
 */
 
 /*
   Add written explanation of your solution here
+  Declare a function called isPalindrome that receives one argument which is the word.
+    Lowercase the string and use RegExp to remove unwanted characters.
+    Use a new variable lowerStr to lowerCase all characters in the string
+    Create a new variable that reverses the string
+    Check if the reversed string is strictly equal to the regular string and return an appropriate boolean
 */
 
 // You can run `node index.js` to view these console logs
